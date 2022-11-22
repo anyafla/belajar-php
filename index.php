c1096af920677b891a9c6a37b205ce1a1bedee2d
Anya Fitriadila Loesita Ayu <anyafla17@gmail.com>
2022-11-22 23:20:19 +0700

    php crud

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+// 1. buat koneksi dengan mysql
+$con = mysqli_connect("localhost", "root", "", "fakultas");
+
+// 2. check connection
+if (mysqli_connect_errno()) {
+    echo "Koneksi gagal" . mysqli_connect_error();
+    exit();
+}
+else {
+    echo "Koneksi berhasil";
+}
+
+// 3. membaca data dari table mysql
+$query ="SELECT * FROM mahasiswa";
+
+// 4. tampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con, $query) ;
+$mahasiswa = [];
+if ($result){
+    // tampilkan data satu per satu
+    while ($row = mysqli_fetch_assoc($result)) {
+        //echo "<br>".$row["nama"]." jenis kelamin : ".$row["jenis_kelamin"];
+    $mahasiswa []=$row;
+    }
+    mysqli_free_result($result);
+}
+
+// 5. tutup koneksi
+mysqli_close($con);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    
+        <table border="1" style="width:100%">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach ($mahasiswa as $value) :?>
+        <tr>
+            <td><?php echo $value ["nim"] ?></td>
+            <td><?php echo $value ["nama"] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    
+</body>
+</html>
